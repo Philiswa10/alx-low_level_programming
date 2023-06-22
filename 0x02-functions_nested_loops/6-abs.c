771407b5a8231851db48b01b535b995a79d74637
Philiswa10 <philiswanqoko@gmail.com>
2023-06-21 18:52:49 -0700

    6-abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _abs - Computers the absolute value
+ * of an integer.
+ *
+ * @i: input number as an integer.
+ *
+ * Return: absolute value
+ */
+
+int _abs(int i)
+
+{
+if (i >= 0)
+{
+return (i);
+}
+else
+{
+	return (i * -1);
+}
+}
--- 0x02-functions_nested_loops/README.md
+++ 0x02-functions_nested_loops/README.md
@@ -1,1 +1,1 @@
-This is a c function project
+This is a project
