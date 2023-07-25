fe27962c7a84571514e3ab65bb6f4a82e9d0ce33
Philiswa10 <philiswanqoko@gmail.com>
2023-07-25 10:52:44 -0700

    1-print_rev_recursion.c

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -3,13 +3,19 @@
 /**
  * _print_rev_recursion - Prints a string in reverse
  * @s: The string to be printed.
+ *
+ * Return: Nothing.
  */
 
 void _print_rev_recursion(char *s)
 {
-	if (*s)
+	if (*s == '\0')
 	{
-		_print_rev_recursion(s + 1);
+		return;
+	}
+
+	s++;
+		_print_rev_recursion(s);
+		s--;
 		_putchar(*s);
 	}
-}
